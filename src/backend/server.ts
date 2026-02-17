73c404d9321a8534e5a72907a52d6ffbece29d49
sarakahveci <yamooonz@gmail.com>
2026-02-17 17:54:57 +0300

    fixed commenting section

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import type { Request, Response } from 'express'
-import { readDB, writeDB } from './db.ts'
+import { readDB, writeDB } from './db'
 
 type Comment = {
   id: string
