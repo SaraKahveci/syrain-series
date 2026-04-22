5e2226c0208ef12768dd5712028a78c23df9185d
sarakahveci <yamooonz@gmail.com>
2026-04-22 13:34:28 +0300

    export db from firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,19 +1,7 @@
-// import { initializeApp } from 'firebase/app'
-
-
-// const firebaseConfig = {
-//   apiKey: 'YOUR_API_KEY',
-//   authDomain: 'YOUR_AUTH_DOMAIN',
-//   projectId: 'YOUR_PROJECT_ID',
-//   appId: 'YOUR_APP_ID',
-// }
-
-// const app = initializeApp(firebaseConfig)
-// 
-// // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from 'firebase/auth'
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyCOddQKpbTQ6Pc4v0HlpGPpHXm3s_RhC6I",
@@ -23,9 +11,10 @@ const firebaseConfig = {
   messagingSenderId: "1052594174245",
   appId: "1:1052594174245:web:8d2fd5f894f664250342cc",
   measurementId: "G-5T3P2BXSDZ"
-};
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 export const auth = getAuth(app)
+export const db = getFirestore(app)
+export const googleProvider = new GoogleAuthProvider()
